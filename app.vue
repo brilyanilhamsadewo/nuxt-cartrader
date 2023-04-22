c8f6e40ddecbac7a31db6cac45abb18eeeefd381
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2023-04-22 20:45:40 +0700

    feat: add background

--- app.vue
+++ app.vue
@@ -9,5 +9,18 @@
       <a href="" class="text-3xl font-mono">cartrader</a>
     </header>
     <!-- NAVBAR END -->
+
+    <!-- HOME HERO START -->
+    <div
+      class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://hips.hearstapps.com/hmg-prod/images/20190923-pcoty-2489-ct-1574785275.jpg?crop=0.566xw:0.566xh;0.228xw,0.296xh&resize=980:*')]"
+    >
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="text-white text-8xl font-serif mb-10">Find your drive</h1>
+        </div>
+      </div>
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+    </div>
+    <!-- HOME HERO END -->
   </div>
 </template>
