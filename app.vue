bb81b657b4d3f3162d62ef7b7acb3c1d19653e09
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2023-04-23 20:32:20 +0700

    feat: add carDetail UI

--- app.vue
+++ app.vue
@@ -1,13 +1,5 @@
 <template>
   <div>
-    <!-- NAVBAR START -->
-    <header
-      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
-    >
-      <a href="" class="text-3xl font-mono">cartrader</a>
-    </header>
-    <!-- NAVBAR END -->
-
 
   </div>
 </template>
